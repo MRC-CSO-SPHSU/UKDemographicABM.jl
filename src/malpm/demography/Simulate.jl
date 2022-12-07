f396a4544a1b37dc56b0809653ffe22675645e2b
Atiyah <a.m.g.elsheikh@gmail.com>
2022-12-07 11:58:15 +0000

    doDivorces

--- src/malpm/demography/Simulate.jl
+++ src/malpm/demography/Simulate.jl
@@ -10,14 +10,13 @@ using MultiAgents: ABM, AbstractMABM, AbstractABMSimulation
 using MultiAgents: allagents, add_agent!, currstep, verbose 
 using MALPM.Demography.Population: removeDead!
 using MALPM.Demography: DemographyExample, LPMUKDemography, LPMUKDemographyOpt
-using MALPM.Models # no need for explicit listing anything(model) is from there
-import MALPM.Demography: allPeople  
+#import MALPM.Models # no need for explicit listing anything(model) is from there
 using SocioEconomics
-import SocioEconomics.Specification.SimulateNew: doDeaths!, doBirths! #, doDivorces!
-# export doDeaths!,doBirths!
+import SocioEconomics.Specification.SimulateNew: doDeaths!, doBirths!, doDivorces!
+export doDeaths!, doBirths!, doDivorces!
 
-alivePeople(model,::LPMUKDemography) = allPeople(model)
-alivePeople(model,::LPMUKDemographyOpt) = alivePeople(model)
+#alivePeople(model,::LPMUKDemography) = allPeople(model)
+#alivePeople(model,::LPMUKDemographyOpt) = alivePeople(model)
        
 
 function removeDeads!(deadpeople,pop,::LPMUKDemography)    
@@ -32,9 +31,7 @@ removeDeads!(deadpeople,pop,::LPMUKDemographyOpt) = nothing
 
 function doDeaths!(model::AbstractMABM, sim::AbstractABMSimulation, example::DemographyExample) # argument simulation or simulation properties ? 
 
-    (deadpeople) = doDeaths!(
-            model,
-            currstep(sim))
+    (deadpeople) = doDeaths!(model,currstep(sim))
     
     # ToDo separate step? 
     removeDeads!(deadpeople,model.pop,example)
@@ -54,23 +51,15 @@ function doBirths!(model::AbstractMABM, sim::AbstractABMSimulation, example::Dem
     nothing 
 end
 
-#=
+
 function doDivorces!(model::AbstractMABM, sim::AbstractABMSimulation, example::DemographyExample) 
 
-    population = model.pop 
-
-    SocioEconomics.Specification.Simulate.doDivorces!(
-                        #allagents(population),
-                        alivePeople(population,example),
-                        currstep(sim),
-                        houses(model),
-                        towns(model),
-                        population.parameters.divorcepars) 
+    doDivorces!(model, currstep(sim)) 
 
     nothing 
 end
 
 
-=# 
+
 
 end # Simulate 
