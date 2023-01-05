50c50508b423342f3bab565cefa641204f39fc26
Atiyah <a.m.g.elsheikh@gmail.com>
2023-01-05 11:41:41 +0000

    tuning

--- src/malpm/demography/Simulate.jl
+++ src/malpm/demography/Simulate.jl
@@ -9,55 +9,65 @@ using MultiAgents:  add_agent!, currstep
 using MALPM.Demography.Population: removeDead!
 using MALPM.Demography: DemographyExample, LPMUKDemography, LPMUKDemographyOpt
 using SocioEconomics
-using SocioEconomics.API.Traits: FullPopulation, NoReturn
+using SocioEconomics.XAgents: Person
+using SocioEconomics.API.Traits: FullPopulation, SimProcess, Death, Birth, Marriage
+using SocioEconomics.Utilities: date2yearsmonths
 import SocioEconomics.Specification.SimulateNew: dodeaths!, dobirths!, 
                         dodivorces!, domarriages!,
                         doAgeTransitions!, doWorkTransitions!, doSocialTransitions!,  
                         doAssignGuardians!
-       
+ 
 
-function doDeaths!(model::AbstractMABM, sim::AbstractABMSimulator, ::LPMUKDemography) 
-    # (; deadsind) = dodeaths!(model,currstep(sim))
-    # ToDo separate step? 
-    # for ind in Iterators.reverse(deadsind)
-    #    removeDead!(ind,model.pop)
-    # end
-    dodeaths!(model, currstep(sim))
+const retDeath = Person[]
+
+_init_return(::LPMUKDemographyOpt,::SimProcess) = 0
+_init_return(::LPMUKDemography,::Death) = retDeath 
+
+_dodeaths!(ret,model,time,::LPMUKDemography) = dodeaths!(model,time,FullPopulation(),ret) 
+_dodeaths!(ret,model,time,::LPMUKDemographyOpt) = dodeaths!(model,time,ret) 
+
+function doDeaths!(model, sim, example) 
+    ret = _init_return(example,Death())
+    ret = _dodeaths!(ret, model, currstep(sim), example)
+    #=years, months = date2yearsmonths(currstep(sim))
+    @info "# of deaths $(years) $(months+1) : $(length(ret.people))"=#
     nothing 
 end # function doDeaths!
 
-function doDeaths!(model, sim, ::LPMUKDemographyOpt) 
-    dodeaths!(model,currstep(sim),FullPopulation(),NoReturn())
-    nothing 
-end # function doDeaths!
+const retBirth = Person[]
+_init_return(::LPMUKDemography,::Birth) = retBirth 
 
-_dobirths!(model,sim,::LPMUKDemography) = dobirths!(model, currstep(sim))  
-_dobirths!(model,sim,::LPMUKDemographyOpt) = 
-    dobirths!(model, currstep(sim),FullPopulation(),NoReturn()) 
+_dobirths!(ret,model,sim,::LPMUKDemographyOpt) = dobirths!(model, currstep(sim), ret)  
+_dobirths!(ret,model,sim,::LPMUKDemography) = 
+    dobirths!(model, currstep(sim), FullPopulation(),ret) 
 
 function doBirths!(model, sim, example)  
-    #(;babies) = _dobirths!(model, sim, example) 
-    # TODO separate step? 
-    # for baby in babies
-    #    add_agent!(model.pop,baby)
-    #end
-    _dobirths!(model, sim, example)
+    ret = _init_return(example,Birth())
+    ret = _dobirths!(ret,model, sim, example)
     nothing 
 end
 
-_dodivorces!(model,sim,::LPMUKDemography) = dodivorces!(model, currstep(sim))  
-_dodivorces!(model,sim,::LPMUKDemographyOpt) = dodivorces!(model, currstep(sim),FullPopulation(), NoReturn()) 
+const retBirth = Person[] 
+_init_return(::LPMUKDemography,::Birth) = retBirth 
+
+_dodivorces!(ret,model,sim,::LPMUKDemographyOpt) = dodivorces!(model, currstep(sim), ret)  
+_dodivorces!(ret,model,sim,::LPMUKDemography) = dodivorces!(model, currstep(sim),FullPopulation(),ret) 
 
 function doDivorces!(model, sim, example) 
-    _dodivorces!(model, sim, example) 
+    ret = _init_return(example, Birth())
+    ret = _dodivorces!(ret, model, sim, example) 
     nothing 
 end
 
-_domarriages!(model,sim,::LPMUKDemography) = domarriages!(model, currstep(sim))  
-_domarriages!(model,sim,::LPMUKDemographyOpt) = domarriages!(model, currstep(sim),FullPopulation(), NoReturn()) 
+_domarriages!(ret,model,sim,::LPMUKDemographyOpt) = domarriages!(model, currstep(sim))  
+_domarriages!(ret,model,sim,::LPMUKDemography) = domarriages!(model, currstep(sim),FullPopulation(), ret) 
+
+const retMarriage = Person[] 
+_init_return(::LPMUKDemography,::Marriage) = retMarriage 
 
 function doMarriages!(model::AbstractMABM, sim::AbstractABMSimulator, example::DemographyExample) 
-    _domarriages!(model, sim,example) 
+    ret = _init_return(example, Marriage())
+    _domarriages!(ret,model, sim,example) 
     nothing 
 end
 
@@ -90,9 +100,5 @@ function doSocialTransitions!(model::AbstractMABM, sim::AbstractABMSimulator, ex
 
     nothing 
 end
-
-
-
-
 
 end # Simulate 
