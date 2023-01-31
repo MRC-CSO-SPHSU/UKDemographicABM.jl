f23e57ba8b35da87e1a8deea7ebef4984d4debe6
Atiyah <a.m.g.elsheikh@gmail.com>
2023-01-31 13:52:39 +0000

    Initial agents.jl example

---
+++ mainAgents.jl
@@ -0,0 +1,28 @@
+include("libspath.jl")
+add_to_loadpath!("../MultiAgents.jl")
+
+using Agents
+using SocioEconomics.XAgents: Person, PersonHouse
+using SocioEconomics.ParamTypes
+using SocioEconomics.Specification.Create
+using SocioEconomics.Specification.Initialize
+
+const simPars = SimulationPars()
+const dataPars = DataPars()
+const pars = DemographyPars()
+
+# significant parameters
+simPars.seed = 0; ParamTypes.seed!(simPars)
+simPars.verbose = false
+simPars.checkassumption = false
+simPars.sleeptime = 0
+pars.poppars.initialPop = 500 # 28100 for 1-min simulation
+
+const data = load_demography_data(dataPars)
+
+const ukTowns = create_inhabited_towns(pars)
+const ukHouses = PersonHouse[]
+const ukPop = create_pyramid_population(pars)
+
+# to fix
+model = AgentBasedModel(Person,Vector{PersonHouse})
