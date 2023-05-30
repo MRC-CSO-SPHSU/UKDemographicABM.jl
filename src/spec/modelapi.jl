895910adc40b98f9c31d5411e26228bb24074c86
Atiyah <a.m.g.elsheikh@gmail.com>
2023-05-30 16:45:22 +0100

    default implementation moved to SE

--- src/spec/modelapi.jl
+++ src/spec/modelapi.jl
@@ -1,23 +1,9 @@
 using Memoization
 
-import SocioEconomics.API.ParamFunc: all_pars, population_pars, birth_pars, divorce_pars,
-    marriage_pars, work_pars, map_pars
-import SocioEconomics.API.ModelFunc: all_people, alive_people,
-    data_of, houses, towns,
-    add_person!, add_house!, remove_person!,
-    currenttime
+using SocioEconomics.API.ModelFunc
+import SocioEconomics.API.ModelFunc: all_people, alive_people, houses, towns,
+    add_person!, add_house!, remove_person!
 import SocioEconomics.API.ModelFunc: share_childless_men, eligible_women
-
-data_of(model) = model.data
-currenttime(model)::Rational{Int} = model.t
-
-all_pars(model) = model.parameters
-population_pars(model) = model.parameters.poppars
-birth_pars(model) = model.parameters.birthpars
-divorce_pars(model) = model.parameters.divorcepars
-marriage_pars(model) = model.parameters.marriagepars
-work_pars(model) = model.parameters.workpars
-map_pars(model) = model.parameters.mappars
 
 _alive_people(model) =
     [ person for person in all_people(model)  if alive(person) ]
